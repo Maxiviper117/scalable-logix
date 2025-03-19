bf7f25e8476522684bbf227aa8329a9fae4675f1
David <davidgonsal@gmail.com>
2025-03-19 20:38:25 +0200

    feat: add ButtonLink, Footer, and Nav components

---
+++ src/lib/components/ButtonLink.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let props: {
+		href: string;
+		text: string;
+		class?: string;
+	} = $props();
+</script>
+
+<a
+	href={props.href}
+	class={[
+		'text-text-light  bg-gradient-to-r to-indigo-500 from-purple-600 hover:shadow-button-hover-shadow inline-block rounded-lg px-8 py-3 font-medium shadow-lg transition-all duration-300 hover:bg-indigo-700 active:scale-95 hover:scale-105',
+		props.class
+	]}
+>
+	{props.text}
+</a>
---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let props = $props();
+</script>
+
+{#snippet navLink(text: string, href: string)}
+	<li>
+		<a class="text-text-light hover:text-accent" {href}>{text}</a>
+	</li>
+{/snippet}
+
+<nav class="bg-background">
+	<ul class="flex items-center justify-center gap-10 p-4 text-xl">
+			{@render navLink('Home', '/')}
+			{@render navLink('Portfolio', '/#/portfolio')}
+			{@render navLink('Contact', '/#/contact')}
+		</ul>
+</nav>
